ee2cc63efa923b536fb16c7c0377dcc1645cd5c9
Scott Nix <scott@scottnix.com>
2012-10-06 23:57:24 -0700

    Removed jQuery addclass clearfix, use CSS clearfix instead.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,3 @@
-// add the class of 'cf' to use the micro-clearfix hack
-// http://nicolasgallagher.com/micro-clearfix-hack/
-jQuery("#access, #main").addClass('cf');
-
-
 // apply fitvids functionality to .entry-content div
 // https://github.com/davatron5000/FitVids.js
 jQuery(".entry-content").fitVids();
