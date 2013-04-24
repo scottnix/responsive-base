b05458cef21e09d8d9008a8d9529505c8badae80
Scott Nix <scott@scottnix.com>
2013-04-24 16:11:11 -0700

    Added Compass config.rb file per request

---
+++ config.rb
@@ -0,0 +1,13 @@
+# compass config.rb refrences - http://compass-style.org/help/tutorials/configuration-reference/
+
+# path to file structures
+css_dir = ""
+sass_dir = ""
+images_dir = "imgages"
+javascripts_dir = "js"
+
+# output_style options - :nested, :expanded, :compact, :compressed
+output_style = :expanded
+
+# line_comments options - true, false
+line_comments = false
